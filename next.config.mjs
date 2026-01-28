ed0689c2a9e8bb713b58828a8e45f941167cfe46
wiseandy32 <wiseandy32@gmail.com>
2026-01-28 08:47:05 +0100

    chore: downgrade react and next, add react-email, resend and firebase admin as dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {},
+  experimental: {
+    turbo: {},
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(mp4|webm)$/,
